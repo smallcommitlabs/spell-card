16cfc1dcd8a6551d1790588f5deaf99239cf4bc6
Kirsty Gong <kirstygong23@gmail.com>
2020-12-16 10:41:22 +1300

    refactor: added option to inptut font size

--- src/phaser/spellCard/components/naviButton.js
+++ src/phaser/spellCard/components/naviButton.js
@@ -17,10 +17,11 @@ export default class naviButton {
      * @param {number} width 
      * @param {number} height 
      * @param {string} name 
+     * @param {number} size
      * @param {string} sceneToNaviTo
      */
-    createBtn(width, height, name,sceneToNaviTo){
-        const btn=this.gameObject.add.text(width,height,name,{fontSize:48}).setOrigin(0.5)
+    createBtn(width, height, size,name,sceneToNaviTo){
+        const btn=this.gameObject.add.text(width,height,name,{fontSize:size}).setOrigin(0.5)
         const navi=new NaviComponent()
         navi.navigationToScene(sceneToNaviTo,this.gameObject.scene,btn);
         return btn
