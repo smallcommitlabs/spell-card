940a9f7010be7e6402a0c8449d3800845406cff0
Kirsty Gong <kirstygong23@gmail.com>
2020-12-16 11:33:05 +1300

    feat: added components in cardCollection
    Added mock cards, decks and new deck in cardCollection

--- src/phaser/spellCard/scenes/cardCollection.js
+++ src/phaser/spellCard/scenes/cardCollection.js
@@ -4,8 +4,10 @@ import NaviButton from "../components/naviButton"
 
 export default class cardCollection extends Phaser.Scene{
 
+
     constructor(){
         super('cardCollection')
+        this.navibutton=new NaviButton(this);
     }
 
     create(){
@@ -14,10 +16,33 @@ export default class cardCollection extends Phaser.Scene{
         this.add.text(width * 0.5, 50, 'Card Collection', { fontSize: 62 })
             .setOrigin(0.5)
         
-        const navibuttion=new NaviButton(this);
-        navibuttion.createBtn(48,14,24,"Return","mainMenu");
+        this.navibutton.createBtn(48,14,24,"Return","mainMenu");
 
+        this.loadCards()
+        this.loadDeck()
+
+        this.navibutton.createBtn(width*0.5,height*0.75,32,"New Deck","")
         
+    }
+
+    loadDeck(){
+        const {width,height}=this.scale
+        let xper=0.25;
+
+        for(let col=0; col<3;++col){
+            this.add.text(width*xper,height*0.5,"Deck",{fontSize:24});
+            xper+=0.25
+        }
+    }
+
+    loadCards(){
+        const {width,height}=this.scale
+        let xper=0.25;
         
+
+        for(let col=0; col<3;++col){
+            this.navibutton.createBtn(width*xper,height*0.25,24,"card","");
+            xper+=0.25
+        }
     }
 }
