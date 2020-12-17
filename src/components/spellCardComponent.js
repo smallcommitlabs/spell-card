988d4f1eaf1f634324abaee989ac80e15f62ef39
kche356 <kche356@aucklanduni.ac.nz>
2020-12-17 15:57:05 +1300

    Added assets for cards

--- src/components/spellCardComponent.js
+++ src/components/spellCardComponent.js
@@ -1,12 +1,12 @@
 import Phaser from "phaser";
-import playGame from "../phaser/spellCard/scenes/GameSecnes/mainGame";
+import playGame from "../phaser/spellCard/scenes/GameScenes/mainGame";
 import preloader from "../phaser/spellCard/scenes/preloader"
 import mainMenu from "../phaser/spellCard/scenes/mainMenu"
 import cardCollection from "../phaser/spellCard/scenes/cardCollection"
-import deckDetail from "../phaser/spellCard/scenes/cardCollectiobScenes/deck"
-import newDeck from "../phaser/spellCard/scenes/cardCollectiobScenes/newDeck"
-import gamePreloader from "../phaser/spellCard/scenes/GameSecnes/gamePreloader"
-import deckSelection from "../phaser/spellCard/scenes/GameSecnes/deckSelection"
+import deckDetail from "../phaser/spellCard/scenes/cardCollectionScenes/deck"
+import newDeck from "../phaser/spellCard/scenes/cardCollectionScenes/newDeck"
+import gamePreloader from "../phaser/spellCard/scenes/GameScenes/gamePreloader"
+import deckSelection from "../phaser/spellCard/scenes/GameScenes/deckSelection"
 import React from "react";
 
 export const config = {
