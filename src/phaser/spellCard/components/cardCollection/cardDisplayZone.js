445a12027029a18f4f9d83afb8c41cdee5fd63ae
Kirsty Gong <kirstygong23@gmail.com>
2020-12-17 16:13:37 +1300

    refactor: rename deck.js

--- src/phaser/spellCard/components/cardCollection/cardDisplayZone.js
+++ src/phaser/spellCard/components/cardCollection/cardDisplayZone.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser'
 import Zone from "../../components/zone"
 
+
+
 export default class cardDisplayZone {
 
     constructor(scene){
@@ -11,9 +13,14 @@ export default class cardDisplayZone {
     create(){
         const {width,height}=this.scene.scale
         const zone=this.zone.creactZone(width*0.1,height*0.2,width*0.45,height*0.55)
+        console.log(zone)
     }
 
     update(){
-        
+
+    }
+
+    mockCards(){
+
     }
 }
