25bf9d00525b3129c4ce795a97115eab686ed646
Kirsty Gong <kirstygong23@gmail.com>
2020-12-20 17:14:28 +1300

    feat: implemented the countdown component

---
+++ src/phaser/spellCard/components/countdownController.js
@@ -0,0 +1,65 @@
+export default class countdownController{
+
+    /** @type {Phaser.Time.TimerEvent} */
+	timerEvent
+
+    /** @type {Phaser.Scene} */
+	scene
+
+	/** @type {Phaser.GameObjects.Text} */
+	label
+
+	/**
+	 * 
+	 * @param {Phaser.Scene} scene 
+	 * @param {Phaser.GameObjects.Text} label 
+	 */
+	constructor(scene, label)
+	{
+		this.scene = scene
+		this.label = label
+    }
+    
+    /**
+	 * @param {() => void} callback
+	 * @param {number} duration 
+	 */
+	start(callback, duration )
+	{
+        this.duration=duration
+        this.stop()
+
+        this.timerEvent=this.scene.time.addEvent({
+            delay:duration,
+            callback:()=>{
+                this.label.text='0'
+
+                this.stop()
+
+                if(callback){
+                    callback()
+                }
+            }
+        })
+	}
+
+	stop()
+	{
+        if(!this.timerEvent || this.duration<=0){
+            return
+        }
+
+        const elapsed=this.timerEvent.getElapsed()
+
+        const remaining=this.duration-elapsed
+
+        const seconds=remaining/1000
+
+        this.label.text=seconds.toFixed(0)
+	}
+
+	update()
+	{
+	}
+
+}
