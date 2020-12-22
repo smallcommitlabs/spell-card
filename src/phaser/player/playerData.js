b0295327d2e71bf8b534f25a2a2c75e46c243bda
Kirsty Gong <kirstygong23@gmail.com>
2020-12-22 21:33:15 +1300

    feat: changed to store all cards inton same array

--- src/phaser/player/playerData.js
+++ src/phaser/player/playerData.js
@@ -5,50 +5,36 @@ import MagicCard from '../components/Cards/magicCard';
 
 export default class cards {
   /** @type {Array} */
-  attackCards;
-
-  /** @type {Array} */
-  defenceCard;
-
-  /** @type {Array} */
-  magicCard;
+  card;
 
   constructor() {
     if (!!cards.instance) {
       return cards.instance;
     }
     cards.instance = this;
-    this.attackCards = new Array();
-    this.defenceCard = new Array();
-    this.magicCard = new Array();
+    this.card = new Array();
 
     return this;
   }
 
   loadCard() {
     let a = 0;
-    let j = 0;
-    let z = 0;
+
     for (const i of Deck.card) {
       let card = null;
       if (i.class === 'Attack') {
-        console.log(i.class);
         card = new AttackCard(i);
-        this.attackCards[a] = card;
-        a++;
       } else if (i.class === 'Defence') {
         card = new DefenceCard(i);
-        this.defenceCard[j] = card;
-        j++;
       } else if (i.class === 'Magic') {
         card = new MagicCard(i);
-        this.magicCard[z] = card;
-        z++;
       }
+      this.card[a] = card;
+      a++;
     }
   }
 
   getRandomCard(numOfCards) {
-    return this.magicCard[0];
+    return this.card[6];
   }
 }
