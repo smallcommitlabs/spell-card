9823c6d93e969495234f8c4fcbb8b65c9df62a0b
Kirsty Gong <kirstygong23@gmail.com>
2020-12-22 20:10:34 +1300

    feat: created the playerData
    Its a singleton class and it contains method to load cards

--- src/phaser/player/playerData.js
+++ src/phaser/player/playerData.js
@@ -1,3 +1,38 @@
-// export default cardObject;
+import Deck from './player';
+import AttackCard from '../components/Cards/attackCard';
+import DefenceCard from '../components/Cards/defenseCard';
+import MagicCard from '../components/Cards/magicCard';
 
-// export default cardobjectCreate;
+export default class cards {
+  /** @type {Map} */
+  cards;
+
+  constructor() {
+    if (!!cards.instance) {
+      return cards.instance;
+    }
+    cards.instance = this;
+    this.cards = new Map();
+
+    return this;
+  }
+
+  loadCard() {
+    // console.log(deck.card[0]);
+    for (const i of Deck.card) {
+      let card = null;
+      if (i.class === 'Attack') {
+        card = new AttackCard(i);
+      } else if (i.class === 'Defence') {
+        card = new DefenceCard(i);
+      } else if (i.class === 'Magic') {
+        card = new MagicCard(i);
+      }
+      this.cards.set(i.name, card);
+    }
+  }
+
+  getCard() {
+    return this.cards;
+  }
+}
