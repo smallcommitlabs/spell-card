05b5ff803eb45f7361afac517fb56e2a20bd4439
Kirsty Gong <kirstygong23@gmail.com>
2020-12-22 21:23:25 +1300

    feat: changed the data storage for card

--- src/phaser/player/playerData.js
+++ src/phaser/player/playerData.js
@@ -4,35 +4,51 @@ import DefenceCard from '../components/Cards/defenseCard';
 import MagicCard from '../components/Cards/magicCard';
 
 export default class cards {
-  /** @type {Map} */
-  cards;
+  /** @type {Array} */
+  attackCards;
+
+  /** @type {Array} */
+  defenceCard;
+
+  /** @type {Array} */
+  magicCard;
 
   constructor() {
     if (!!cards.instance) {
       return cards.instance;
     }
     cards.instance = this;
-    this.cards = new Map();
+    this.attackCards = new Array();
+    this.defenceCard = new Array();
+    this.magicCard = new Array();
 
     return this;
   }
 
   loadCard() {
-    // console.log(deck.card[0]);
+    let a = 0;
+    let j = 0;
+    let z = 0;
     for (const i of Deck.card) {
       let card = null;
       if (i.class === 'Attack') {
+        console.log(i.class);
         card = new AttackCard(i);
+        this.attackCards[a] = card;
+        a++;
       } else if (i.class === 'Defence') {
         card = new DefenceCard(i);
+        this.defenceCard[j] = card;
+        j++;
       } else if (i.class === 'Magic') {
         card = new MagicCard(i);
+        this.magicCard[z] = card;
+        z++;
       }
-      this.cards.set(i.name, card);
     }
   }
 
-  getCard() {
-    return this.cards;
+  getRandomCard(numOfCards) {
+    return this.magicCard[0];
   }
 }
