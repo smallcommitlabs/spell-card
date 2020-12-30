6e42c01159f9ecde417db6ef579f04a98e024464
Alexander Nicholson <alexn400@gmail.com>
2020-12-30 17:19:48 +1300

    Setup auth slice in reudx with existing auth client

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+import AuthSlice from './auth/AuthSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: AuthSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
