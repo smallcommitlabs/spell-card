d07045cf2da2a68d9c1decf931947b6a79aa3378
kche356 <kche356@aucklanduni.ac.nz>
2021-01-08 22:01:45 +1300

    feat - added punishment for incorrectly answering or not answering a card

--- src/phaser/scenes/GameScenes/mainGame.js
+++ src/phaser/scenes/GameScenes/mainGame.js
@@ -21,6 +21,8 @@ export default class playGame extends Phaser.Scene {
   }
 
   create() {
+    this.correctCards = new Array();
+
     console.log(this.scene);
     const { width, height } = this.scale;
     this.add
@@ -66,6 +68,7 @@ export default class playGame extends Phaser.Scene {
         const mainGameTimerLabel = data.mainGameCounter;
         // restart the timer
         mainGameTimerLabel.resume(timeRemain);
+        this.correctCards = data.correct;
       }
     });
 
@@ -91,7 +94,7 @@ export default class playGame extends Phaser.Scene {
   }
 
   // Creates the pop-up screen
-  popUpScreen(button, popUpName, popUpInput, data, callback) {
+  popUpScreen(button, popUpName, popUpInput, data, callback, card) {
     button.on(
       'pointerdown',
       function () {
@@ -100,6 +103,8 @@ export default class playGame extends Phaser.Scene {
           counter: this.countdown,
           timerLabel: this.timerLabel,
           question: data,
+          correct: this.correctCards,
+          card: card,
           key: 'game',
           callback: callback.bind(this),
         });
@@ -117,7 +122,8 @@ export default class playGame extends Phaser.Scene {
     this.scene.start('roundResult', {
       player1Health: this.player1Health,
       player2Health: this.player2Health,
-      cards: this.selectedCards,
+      cards: this.correctCards,
+      length: this.selectedCards.length,
     });
   }
 
@@ -133,9 +139,16 @@ export default class playGame extends Phaser.Scene {
       x += 122;
 
       // Add popup question board screen to card
-      this.popUpScreen(card, 'questionBoard', QuestionBoard, x, () => {
-        card.disableInteractive();
-      });
+      this.popUpScreen(
+        card,
+        'questionBoard',
+        QuestionBoard,
+        x,
+        () => {
+          card.disableInteractive();
+        },
+        i
+      );
     }
   }
 }
