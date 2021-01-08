5b8868eed7c852412744d75f77b45f3627ccd264
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2021-01-08 18:44:51 +1300

    Game logic (#11)
    * refactor: changed the constructor param
    
    change the constructor param to be an array
    
    * refactor: changed the name of each card
    
    * feat: created the playerData
    
    Its a singleton class and it contains method to load cards
    
    * feat: load cards
    
    * feat: added getCard() method
    
    * feat: changed the data storage for card
    
    * feat: changed to store all cards inton same array
    
    * debug: fixed the class name
    
    * feat: added method to get random cards
    
    * fix: fixed the value assign for useCardused
    
    * refactor changed the menuButtonFuntion()
    
    Made menuButtonFuntion() reusable
    
    * feat: layout for the question board
    
    * feat: implemeted the popup screen for questioBoard
    
    * feat: added format for time over a minute
    
    * fix: fixed the minute of the timer
    
    * feat: moved the timer to mainGame
    
    * feat: added getReman()
    
    Return the remaning time
    
    * feat: changed resume()
    
    change resume() to restart the timer with a given time
    
    * feat: implemented timer
    
    implemented consistent timer across the main game and the popup screens
    
    * doc: added comments for explaination
    
    * feat: pass the selected cards to mainGame
    
    * feat: added pop-up screen for cards
    
    * feat: display question
    
    * feat: added roundResult scene
    
    This scene is for displaying the battle animations
    
    * refactor: make the code more reflexive
    
    The code is refactor to use for both mainGame and roundResult
    
    * fix: fixed to handle the mainDame and roundResult
    
    * fix: removed not necessary codes
    
    * feat: added navigation to roundResults
    
    * feat: added navigation to roundResult
    
    When the timer is up, it will navigates to roundResult scene
    
    * feat: added navigation to roundResult
    
    When the timer is up, it will navigates to roundResult scene
    
    * feat: created health class
    
    * feat: added player
    
    * feat: added health display
    
    * fix: correctly store the callback function
    
    * feat: Pass player health to roundResult
    
    * feat: re-did the navigation to roundResult
    
    Remove direct navigation and go back to mainGame for navigation
    
    * feat: apply effect of the card
    
    * feat: make cards not interative when its confirmed
    
    * doc: commenting
    
    * feat: logic to decide to end game or continue
    
    * fet: navigation to gameResult when game is done
    
    * fix: fixed the bug for the settingbtn in mainGame

--- src/phaser/scenes/GameScenes/mainGame.js
+++ src/phaser/scenes/GameScenes/mainGame.js
@@ -80,7 +80,9 @@ export default class playGame extends Phaser.Scene {
 
     // Timer
     // const time = 300000;
-    const time = 20000;
+    
+    const time = 5000;
+
     this.timerLabel = this.add.text(width * 0.5, 220, '5:00', { fontSize: 32 }).setOrigin(0.5);
     this.countdown = new CountdownController(this, this.timerLabel);
     this.countdown.start(this.handleCountdownFinished.bind(this), time);
@@ -92,6 +94,12 @@ export default class playGame extends Phaser.Scene {
 
   // Creates the pop-up screen
   popUpScreen(button, popUpName, popUpInput, data, callback) {
+
+    let callbackFun = null;
+    if (callback) {
+      callbackFun = callback.bind(this);
+    }
+
     button.on(
       'pointerdown',
       function () {
@@ -101,7 +109,8 @@ export default class playGame extends Phaser.Scene {
           timerLabel: this.timerLabel,
           question: data,
           key: 'game',
-          callback: callback.bind(this),
+          callback: callbackFun,
+
         });
         // hide the timer
         this.timerLabel.visible = false;
