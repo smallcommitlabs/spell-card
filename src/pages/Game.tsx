a1ae7920e52630b4995253bff32661de94c6fa03
Alexander Nicholson <39721828+alexn400@users.noreply.github.com>
2021-01-11 17:15:23 +1300

    Set up Redux 🚀 (#9)
    * Set up redux
    
    * Move types in package.json
    
    * Setup auth slice in reudx with existing auth client
    
    * Update eslint config to remove prettier style conflict
    
    * Refactor login page to use redux

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import SpellCard from '../components/SpellCard';
+import { logout } from '../redux/auth/thunks/logout';
+import { useAppDispatch } from '../redux/store';
 import { AuthClient } from '../util/AuthClient';
 
-const Game = () => (
-  <div style={{ height: '100vh', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-    <h1>Spell Card Logo</h1>
-    <SpellCard />
-    <Button
-      color={'primary'}
-      className={'mt-3'}
-      onClick={() => {
-        AuthClient.signOut();
-        window.location.reload();
-      }}
+const Game = () => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div
+      style={{ height: '100vh', display: 'flex', alignItems: 'center', flexDirection: 'column' }}
     >
-      Logout
-    </Button>
-  </div>
-);
+      <h1>Spell Card Logo</h1>
+      <SpellCard />
+      <Button color={'primary'} className={'mt-3'} onClick={() => dispatch(logout())}>
+        Logout
+      </Button>
+    </div>
+  );
+};
 export default Game;
--- src/util/AuthClient.ts
+++ src/util/AuthClient.ts
@@ -11,34 +11,37 @@ export interface IAuthClient {
    * Attempt to sign in the user with the provided credientials
    * @returns true if the login was accepted otherwise false
    */
-  signIn: (username: string, password: string) => boolean;
+  signIn: (username: string, password: string) => Promise<boolean>;
 
   /**
    * Signs out the user. If the user is not signed in then this will have no effect
    */
-  signOut: () => void;
+  signOut: () => Promise<void>;
 
   /**
    * Check if the user is currently authenticated
    * @returns true if the user is currently authenticated otherwise false
    */
-  isAuth: () => boolean;
+  isAuth: () => Promise<boolean>;
 }
 
 /**
  * Mock implementation of the authentication client that will accept any set of credientials.
  */
 export const AuthClient: IAuthClient = {
-  signIn: (username, password) => {
+  signIn: async (username, password) => {
     console.log(`User is attempting to login with details: ${username}, ${password}`);
     localStorage.setItem('auth', 'true');
     return true;
   },
 
-  signOut: () => {
+  signOut: async () => {
     console.log('Signing out user');
     localStorage.removeItem('auth');
   },
 
-  isAuth: () => localStorage.getItem('auth') !== null,
+  isAuth: async () => {
+    const res = localStorage.getItem('auth') !== null;
+    return res;
+  },
 };
