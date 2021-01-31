1921c1e77b498bf1df3e8b35986dde20c051e544
Kirsty Gong <kirstygong23@gmail.com>
2021-01-31 19:43:57 +1300

    feat: playerAttack update

--- src/phaser/scenes/GameScenes/playerAttack.js
+++ src/phaser/scenes/GameScenes/playerAttack.js
@@ -1,19 +1,20 @@
+import Phaser from 'phaser';
 import GamingScene from '../../components/gamingScene';
 
 export default class playerAttack extends Phaser.Scene {
   init(data) {
     this.player1Health = data.player1Health;
     this.player2Health = data.player2Health;
+    this.currentCard = data.currentCard;
   }
 
   constructor() {
     super('playerAttack');
     this.gamingScene = new GamingScene(this, 'playerAttack');
-    this.currentCard = data.currentCard;
   }
 
   create() {
     const { width, height } = this.scale;
-    this.gamingScene.buildScene(this.player1Health, this.player2Health, true);
+    this.gamingScene.buildScene(this.player1Health, this.player2Health, false);
   }
 }
