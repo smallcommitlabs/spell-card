2c4ed4cef6e2a6cda22891b5b10761ee65d26496
Kirsty Gong <kirstygong23@gmail.com>
2021-02-12 20:32:46 +1300

    feat: added armour feature

--- src/phaser/player/playerStats.js
+++ src/phaser/player/playerStats.js
@@ -3,8 +3,9 @@ export default class playerHealth {
    *
    * @param {number} health
    */
-  constructor(health) {
+  constructor(health, armour) {
     this.health = health;
+    this.armour = armour;
   }
 
   getHealth() {
@@ -17,5 +18,20 @@ export default class playerHealth {
    */
   dealDamage(damage) {
     this.health -= damage;
+  }
+
+  addDefence(value) {
+    this.defenceValue += value;
+  }
+
+  reduceDefence(value) {
+    this.defenceValue -= value;
+    if (this.defenceValue <= 0) {
+      this.defenceValue = 0;
+    }
+  }
+
+  getDefenceValue() {
+    return this.defenceValue;
   }
 }
