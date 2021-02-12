4903e7f198c13828515a93d593893e6ab5df875d
Kirsty Gong <kirstygong23@gmail.com>
2021-02-12 20:26:14 +1300

    refactor: rename playerHealth.js to playerStas.js

--- src/phaser/scenes/GameScenes/mainGame.js
+++ src/phaser/scenes/GameScenes/mainGame.js
@@ -3,7 +3,7 @@ import GamingScene from '../../components/gamingScene';
 import QuestionBoard from '../../components/answerQuestion/questionBoard';
 import SettingMenu from '../../components/gameSetting/settingMenu';
 import CountdownController from '../../components/countdownController';
-import PlayerHealth from '../../player/playerHealth';
+import PlayerHealth from '../../player/playerStats';
 import PlayerData from '../../player/playerData';
 import DojoBoss from '../../boss/DojoBoss';
 
